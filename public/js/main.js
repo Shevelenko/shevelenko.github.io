b31c657e156916f1422ece7b076a0863704c5f75
Vasily Klenov <klenov@gmail.com>
2015-01-22 19:07:17 +0300

    update

--- public/js/main.js
+++ public/js/main.js
@@ -4,13 +4,15 @@ $( document ).ready(function() {
   $("#home").click(function(event){
     event.preventDefault();
     $(".contact_div").hide();
+    $(".how_it_works_div").hide();
     $(".video_div").show();
   });
 
   $("#how_it_works").click(function(event){
     event.preventDefault();
-    // $(".video_div").show();
-    // $(".contact_div").hide();
+    $(".how_it_works_div").show();
+    $(".contact_div").hide();
+    $(".video_div").hide();
   });
 
   $("#who_we_are").click(function(event){
