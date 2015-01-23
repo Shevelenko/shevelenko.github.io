ec7c92ac2a8dbf1b30d67b762e23e77d0f587753
Vasily Klenov <klenov@gmail.com>
2015-01-23 14:01:11 +0300

    js fix

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,14 @@
 $( document ).ready(function() {
   
+  $('.popup-vimeo').magnificPopup({
+    disableOn: 700,
+    type: 'iframe',
+    mainClass: 'mfp-fade',
+    removalDelay: 160,
+    preloader: false,
+
+    fixedContentPos: false
+  });
 
   $("#home").click(function(event){
     event.preventDefault();
@@ -24,7 +33,7 @@ $( document ).ready(function() {
   $("#contact").click(function(event){
     event.preventDefault();
     $(".contact_div").show();
-
+    $(".how_it_works_div").hide();
     $(".video_div").hide();
     
   });
